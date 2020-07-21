6d6be4157aeabf4b354feb39364aa1f5011ccbe0
Rohan Gupta <guptarohan323@gmail.com>
2020-07-21 08:16:24 -0500

    ditched teh tree and node classes. Not necessary to do what I need to do, the json format is pretty simple as it is

--- Objects/Pokemon.py
+++ Objects/Pokemon.py
@@ -3,19 +3,21 @@ Essentially it takes a JSON file of the pokemon's name, and has functions to get
 The JSON file is parsed and separated upon creating that object.
 '''
 from Data.JSON import read
+from Objects.Tree import *
+import json
 class Pokemon():
     '''Takes in only one argument, which is going to be a json file in the format of pokemonname.json. It will parse the json and sort
     them out.
     Optional argument: moveset. If the user wants to provide a moveset, other than that we will assume the moveset is None.
     '''
-    def __init__(self, jsonName, moveset=None):
-        self.json = read(jsonName); #file to be parsed
+    def __init__(self, jsonName:str, moveset=None):
+        self.json = json.loads(jsonName); #file to be parsed
         self.moves = None #a generateor object, to generate all the moves the pokemon has.
         self.abilities =[] #an  list containing ability objects
-        self.types=[] #list of strings containing the types of the pokemon
-        self.name= jsonName.split(".")[0] #string containing the pokemon name
-        self.weight, self.height = -1 #integers containing the weight and height of the pokemon respectively
-        self.stats = None #to be defined data structure to store teh pokemon's stats.
+        self.types=self.json['types'] #list of strings containing the types of the pokemon
+        self.name= self.json['name'] #string containing the pokemon name
+        self.weight, self.height = self.json['weight'], self.json['height'] #integers containing the weight and height of the pokemon respectively
+        self.stats = self.json['stats'] #to be defined data structure to store teh pokemon's stats.
 
 
     #function to get the moves from a json dictionary
@@ -46,3 +48,6 @@ class Pokemon():
     #function to get stats of a pokemon
     def getStats(self):
         pass
+
+
+
