0166c324805ced88b5649ffabc3fe487d629f693
Rohan Gupta <guptarohan323@gmail.com>
2020-08-19 14:40:24 -0500

    Database stuff fixed. need to delete .json files under ./Files later

--- Data/Database.py
+++ Data/Database.py
@@ -29,13 +29,13 @@ def add(data:dict, table_name:str):
         add_pokemon(data)
 
     elif(table_name=="Ability"):
-        add_abillity(data)
+        add_ability(data)
 
     elif(table_name == "Move"):
-        pass
+        add_move(data)
 
     elif(table_name == "Item"):
-        pass
+        add_item(data)
 
 # function to add data to the pokemon table.
 def add_pokemon(data:dict):
@@ -100,16 +100,175 @@ def add_ability(data:dict):
     return data
 
 def add_move(data:dict):
-    pass
+    # check data to see if it matches this format:
+    # damage_class (text) - indicates the type of damage (status or physical), accuracy (int), effect(text), id (int),
+    # power(int), 
+    keys = {'damage_class':str, 'accuracy': int,  'id': int, 'power':int, 'name':str, 'pp':int,
+            'type':str, 'effects': str
+            }
+    check = lambda keys, d: all([k in keys.keys() and type(d[k]) == keys[k] for k in d.keys()])
+    if False == check(keys, data):
+        raise Exception(
+            "Dictionary data not formatted probably. Should be formated like this: {}. Data: {}".format(keys, data))
 
+    # define cursor and connection objects
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    #insert into move table
+    try:
+        cur.execute("""INSERT INTO move VALUES ('{}','{}', {}, {}, '{}', {}, '{}', "{}");""".format(
+        data['damage_class'], data['accuracy'],  data['id'], data['power'], data['name'], data['pp'],
+            data['type'], data['effects']
+        ))
+
+    # allowing user input for exception handling, in the ability case there are some cases where "" are used for the descriptions.
+    except Exception as e:
+        print("An error occured! Exception: {}".format(e))
+        print("Here's the id: {}. Data: {}".format(data['id'], data))
+        effects = input("effects: ")
+        cur.execute("""INSERT INTO move VALUES ('{}','{}', {}, {}, '{}', {}, '{}', "{}");""".format(
+            data['damage_class'], data['accuracy'], data['id'], data['power'], data['name'], data['pp'],
+            data['type'], effects
+        ))
+
+        print("Modifications made successfully to move {}!".format(data['id']))
+
+
+    conn.commit()
+
+    conn.close()
+    print("Move {} was successfully added to the {} table in the {} database".format(data['name'], "Ability", db))
+    return data
+
+#for the items table in database
 def add_item(data:dict):
-    pass
+    # check data to see if it matches this format:
+    # damage_class (text) - indicates the type of damage (status or physical), accuracy (int), effect(text), id (int),
+    # power(int),
+    keys = { 'id': int,  'name': str, 'effect': str
+            }
 
-def search():
-    pass
+    check = lambda keys, d: all([k in keys.keys() and type(d[k]) == keys[k] for k in d.keys()])
+    if False == check(keys, data):
+        raise Exception(
+            "Dictionary data not formatted probably. Should be formated like this: {}. Data: {}".format(keys, data)
+        )
 
-def get():
-    pass
 
-def delete_item():
-    pass
+    # create cursor and conn objects
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    # insert into item table
+    try:
+        cur.execute("""INSERT INTO item VALUES ({},'{}', '{}');""".format(
+           int(data['id']), data['name'], data['effect'].replace("'", "")
+        ))
+
+    except Exception as e:
+        print("Exception {} occured on line 165".format(e))
+        a = input("Would you like to modify the data (y/n): ")
+        if(a=="y"):
+            effects = input("effects: ")
+
+            cur.execute("""INSERT INTO item VALUES ({},'{}', '{}');""".format(
+                int(data['id']), data['name'], effects
+            ))
+
+    # commit command
+    conn.commit()
+
+    # close conn object
+    conn.close()
+
+    print("Successfully entered {} item in item table".format(data['name']))
+
+#returns true if an input is in the table in hte database
+def search(table_name:str , item:dict):
+    try:
+        # create cursor and connection objects
+        conn = sqlite3.connect(db)
+        cur = conn.cursor()
+
+        # execute command
+        cur.execute("""SELECT * FROM {} WHERE {}={}.""".format(table_name, 'id', item['id']))
+
+        # commit command
+        conn.commit()
+
+        return True
+
+    except Exception as e:
+        print("ERROR {} occured in search() function!".format(e))
+        return False
+
+
+# returns an item given input
+def get(table_name:str, item:dict):
+    # Error checking
+    if('id' not in item.keys()):
+        raise Exception("ERROR! item dict doesn't have id as a key")
+
+    if(not search(table_name, item)):
+        raise Exception("ERROR! Item: {} does not exist in table {}".format(item, table_name))
+
+    # create connection and cursor objects
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    # find item
+    cur.execute('''SELECT * FROM {} WHERE id={}'''.format(table_name, item['id']))
+
+    # get the data
+    rows = cur.fetchall()
+
+    # commit changes
+    conn.commit()
+
+    # close objects
+    cur.close()
+    conn.close()
+
+    return rows[0]
+
+# edit item given a table name, id to identify the item and the new item you want to replace
+def edit(table_name:str, id:int, new_data:dict):
+    # define conn and cur objects
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    # generate aString - string that covers the SET part using new_data dict
+    aString = ",".join([str(k)+"="+str(new_data[k]) for k in new_data.keys()])
+    print("edit_item(), line 242: " + aString)
+
+
+    # UPDATE item
+    cur.execute('''UPDATE {} SET {} WHERE id={}'''.format(table_name, aString, id))
+
+    # commit changes
+    conn.commit()
+
+    # close objects
+    cur.close()
+    conn.close()
+
+
+# delete an item given a table name and the item itself
+def delete(id:int, table_name:str):
+    # create conn and cur objects
+    conn = sqlite3.connect(db)
+    cur = conn.cursor
+
+    # execute command
+    cur.execute('''DELETE FROM {} WHERE id={}'''.format(table_name, id))
+
+    # commit changes
+    conn.commit()
+
+    # close objects
+    cur.close()
+    conn.close()
+
+
+
--- Tests/DatabaseTest.py
+++ Tests/DatabaseTest.py
@@ -60,6 +60,159 @@ class MyTestCase(unittest.TestCase):
             
         self.assertEqual(True, True)
 
+    #test for pokemon moves
+    def test_loadmovejson(self):
+        #load all the json into ./Files/Moves/ from PokeAPI
+        path = "C:\\Users\\gupta\\PycharmProjects\\PokemonProject\\Files\\Move\\"
+        i=1
+        while(i>0):
+            try:
+                print("Loading move json with id: {}".format(i))
+                get_json(type='Move', i=i)
+            except Exception as e:
+                print("An error occured! Exception: {}".format(e))
+                break
+            i+=1 #this is unnecessary
+
+    # adding moves in the ./Files/Move directory into the database
+    def test_addmove(self):
+        path = "C:\\Users\\gupta\\PycharmProjects\\PokemonProject\\Files\\Move\\"
+        moves = os.listdir(path)
+
+
+        for move in moves:
+            move_dict = JSON.read(path+move)
+
+            # assemble data dictionary
+            data = dict()
+            keys = {'damage_class':str, 'accuracy': int, 'id': int, 'power':int, 'name':str, 'pp':int,
+            'type':str, 'effects': str}
+
+            # define damage_class, accuracy, id
+            data['damage_class'], data['accuracy'] = move_dict['damage_class']['name'], move_dict['accuracy']
+            if(data['accuracy']==None):
+                data['accuracy'] = 100
+                
+            # define id, power, name
+            try:
+                if(move_dict['power']==None or move_dict['power']=='None'):
+                    data['id'], data['power'], data['name'] = int(move_dict['id']), 0, move_dict['name']
+                else:
+                    data['id'], data['power'], data['name'] = int(move_dict['id']), int(move_dict['power']), move_dict['name']
+            except Exception as e:
+                print("ERROR {} occured in line 97".format(e))
+                print("Here's move_dict: " + str(move_dict))
+                a = input("Would you like to alter the data? Type y or n: ")
+                if(a=='y'):
+                    id, power, name = input("id: "), input("power: "), input("name: ")
+                    data['id'], data['power'], data['name'] = int(id), int(power), name
+
+            # define type, pp
+            try:
+                data['type'], data['pp'] = move_dict['type']['name'], move_dict['pp']
+
+            except Exception as e:
+                print("ERROR {} occured on line 108!".format(e))
+                a = input("Would you like to alter the data? Type y or n: ")
+                if (a == 'y'):
+                    type, pp = input("type: "), input("pp: ")
+                    data['type'], data['pp'] = type, pp
+
+            # define effects - making sure its in english
+            try:
+                data['effects'] = [effect['effect'] for effect in move_dict['effect_entries'] if(effect['language']['name']=='en')][0]
+
+            except Exception as e:
+                print("ERROR {} occured on line 122".format(e))
+                a = input("Would you like to alter the data? Type y or n: ")
+                if (a == 'y'):
+                    effect = input("effect: ")
+                    data['effects'] = [effect]
+
+            # now add the newly data to the database
+            print("Adding {} to the database".format(data))
+            add_move(data)
+
+        self.assertEqual(True, True)
+
+    #test to load json locally
+    def test_loaditemjson(self):
+        # load all the json into ./Files/Moves/ from PokeAPI
+        path = "C:\\Users\\gupta\\PycharmProjects\\PokemonProject\\Files\\item\\"
+        i = 1
+        while (i > 0):
+            try:
+                print("Loading move json with id: {}".format(i))
+                get_json(type='item', i=i)
+            except Exception as e:
+                print("An error occured! Exception: {}".format(e))
+                break
+            i += 1  #this is unnecessary
+
+    #add item to data
+    def test_additem(self):
+        path = "C:\\Users\\gupta\\PycharmProjects\\PokemonProject\\Files\\item\\"
+        items = os.listdir(path)
+
+        for item in items:
+            item_dict = JSON.read(path+item)
+
+            # assemble data dictionary
+            data = dict()
+            keys = {'id': int, 'name': str, 'effects': str
+                    }
+
+            #print("Adding {}".format(item_dict))
+
+            # define id, name
+            try:
+                data['id'], data['name'] = int(item_dict['id']), item_dict['name']
+            except Exception as e:
+                print("ERROR {} occured in line 152 in test_additem() in DatabaseTest.py".format(e))
+                print("Here's the item: {}".format(item_dict))
+
+                a = input("Would you like to modify id and name (y/n): ")
+                if(a == "y"):
+                    id = input("id: ")
+                    name = input("name: ")
+                    data['id'], data['name'] = int(id), name
+
+            # define effect
+            try:
+                data['effect'] = [effect['effect'] for effect in item_dict['effect_entries'] if(effect['language']['name'] == "en")][0]
+            except Exception as e:
+                print("Error {} occured on line 165".format(e))
+                print("Here's the item: {}".format(item_dict))
+                a = input("Would you like to modify the effect? (y/n): ")
+                if(a == 'y'):
+                    effect = input("effect: ")
+                    data['effect'] = effect
+
+
+            # call add_move()
+            print("Adding {} to item table".format(data))
+            add_item(data)
+
+
+        self.assertEqual(True, True)
+
+    def test_gettest(self):
+        tables = ["item", "Move", "Pokemon"]
+        for i in range(1,11):
+            item = {'id':i}
+
+            for table in tables:
+                print(get(table, item))
+
+        self.assertEqual(True, True)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
